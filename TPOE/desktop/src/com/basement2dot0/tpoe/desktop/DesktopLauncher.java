6ee3b0dfbc9a5b935149f222d15b8838fcdcbd25
basement2dot0 <Basement2dot0@gmail.com>
2018-05-20 21:17:50 +0100

    Commit made that changes project strucutre

---
+++ TPOE/desktop/src/com/basement2dot0/tpoe/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.basement2dot0.tpoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.basement2dot0.tpoe.MainGame;
+
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
